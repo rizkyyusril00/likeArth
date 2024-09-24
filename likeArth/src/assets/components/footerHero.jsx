6952272933b26f3950473662cb9975eab89d342b
rizkyyusril00 <rizkyyusril74@gmail.com>
2024-09-24 16:58:19 +0700

    fix bug slider animation

--- likeArth/src/assets/components/footerHero.jsx
+++ likeArth/src/assets/components/footerHero.jsx
@@ -52,7 +52,7 @@ export default function FooterHero(){
                                 <p className="text-[1rem] md:text-[1.4rem] text-[#676767] text-center">{Poins}</p>
                             </div>
                         {/* contact us */}
-                        <div className="px-9 md:px-40 xl:px-[250px] container mx-auto max-w-[1120px] flex items-center justify-center">
+                        <div className="px-7 md:px-40 xl:px-[250px] container mx-auto max-w-[1120px] flex items-center justify-center">
                             <motion.div whileInView={{width: widthAnimation}} transition={{delay: 1.5, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className="bg-white rounded-[0.938rem] pl-[17px] py-1 xl:py-0 justify-between items-center flex relative z-10">
                                 {/* invisible place holder */}
                                 <p className='invisible py-[0.1rem] md:py-[0.6rem] leading-[28px]'>1</p>
@@ -61,13 +61,13 @@ export default function FooterHero(){
                                     <div className="invisible flex items-center justify-center bg-red-500 w-[1rem] h-[1rem] rounded-[50%] isolate">
                                         <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%]"></div>
                                     </div>
-                                    <p className='py-[0.1rem] md:py-[0.6rem] leading-[28px]'><span className="text-primary">1 Slot available!</span> <span className='hidden md:inline-block text-[#676767]'>Let's talk</span> <span className='hidden xl:inline-block text-[#676767]'>to get your project started.</span></p>
+                                    <p className='py-[0.1rem] md:py-[0.6rem] text-[0.875rem] leading-[28px]'><span className="text-primary">1 Slot available!</span> <span className='hidden md:inline-block text-[#676767]'>Let's talk</span> <span className='hidden xl:inline-block text-[#676767]'>to get your project started.</span></p>
                                 </motion.div>
                                 {/* icon pulse */}
                                 <div className="absolute left-[17px] flex items-center justify-center bg-primary bg-opacity-25 w-[1rem] h-[1rem] rounded-[50%] animate-scale-down-infinite">
                                 </div>
                                 <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%] absolute left-[20px]"></div>
-                                <motion.a href="https://cal.com/likearthstudio/30min" target='_blank' className='py-[10px] md:py-[14px] xl:py-3 w-[100px] md:w-[170px] text-center text-white text-[0.8rem] md:text-[1.1rem] xl:text-[1rem] bg-primary hover:bg-[#04525b] transition-all duration-500 ease-in-out rounded-[0.938rem] absolute right-0'>Book a Call</motion.a>
+                                <motion.a href="https://cal.com/likearthstudio/30min" target='_blank' className='py-[10px] md:py-[14px] xl:py-[13px] w-[100px] md:w-[170px] text-center text-white text-[0.875rem] bg-primary hover:bg-[#04525b] transition-all duration-500 ease-in-out rounded-[0.938rem] absolute right-0'>Book a Call</motion.a>
                             </motion.div>
                         </div>
                     </div>
