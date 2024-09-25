0f0123aca939626317896c4bfb0316033400a849
rizkyyusril00 <rizkyyusril74@gmail.com>
2024-09-25 18:19:51 +0700

    finall kah?

--- likeArth/src/assets/components/hero.jsx
+++ likeArth/src/assets/components/hero.jsx
@@ -82,15 +82,15 @@ export default function Hero() {
                             </motion.div>
                         </motion.div>
                         {/* contack us mobile */}
-                        <motion.div whileInView={{y: [25, 0] }} transition={{delay: 0.9, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className='flex md:hidden justify-center items-center'>
-                            <motion.div whileInView={{opacity: [0, 1] }} transition={{delay: 1, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className="flex items-center justify-center gap-1 md:gap-4 xl:gap-[0.625rem] bg-white pl-[0.75rem] rounded-[0.938rem] relative w-auto transition-all duration-500 ease-in-out">
+                        <motion.div whileInView={{y: [25, 0] }} transition={{delay: 0.9, duration: 1.5, ease: "easeInOut" }} viewport={{ once: false }} className='flex md:hidden justify-center items-center'>
+                            <motion.div whileInView={{opacity: [0, 1] }} transition={{delay: 1, duration: 1.5, ease: "easeInOut" }} viewport={{ once: false }} className="flex items-center justify-center gap-1 md:gap-4 xl:gap-[0.625rem] bg-white pl-[0.75rem] rounded-[0.938rem] relative w-auto transition-all duration-500 ease-in-out">
                                 <div className='flex items-center justify-center gap-[0.625rem]'>
                                     {/* icon pulse invis */}
                                     <div className="invisible flex items-center justify-center bg-red-500 w-[1rem] h-[1rem] rounded-[50%] isolate">
                                         <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%]"></div>
                                     </div>
                                     {/*attention */}
-                                    <motion.p  whileInView={{opacity: [0, 1], fontSize: ['0.1rem', '0.875rem'] }} transition={{delay: 2.2, duration: 1, ease: "easeInOut" }} viewport={{ once: true }} className='py-[0.1rem] md:py-[0.6rem] leading-[28px] mr-2'><span className="text-primary">1 Slot available!</span> <span className='hidden md:inline-block text-[#676767]'>Let's talk</span> <span className='hidden xl:inline-block text-[#676767]'>to get your project started.</span></motion.p>
+                                    <motion.p  whileInView={{opacity: [0, 1], fontSize: ['0.1rem', '0.875rem'] }} transition={{delay: 2.2, duration: 1, ease: "easeInOut" }} viewport={{ once: false }} className='py-[0.1rem] md:py-[0.6rem] leading-[28px] mr-2'><span className="text-primary">1 Slot available!</span> <span className='hidden md:inline-block text-[#676767]'>Let's talk</span> <span className='hidden xl:inline-block text-[#676767]'>to get your project started.</span></motion.p>
                                 </div>
                                 {/* icon pulse */}
                                 <div className="absolute left-[0.75rem] flex items-center justify-center bg-primary bg-opacity-25 w-[1rem] h-[1rem] rounded-[50%] animate-scale-down-infinite">
