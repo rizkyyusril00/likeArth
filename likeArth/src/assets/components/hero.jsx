838dac739d1dae36442cb99f876688149f04cbe4
rizkyyusril00 <rizkyyusril74@gmail.com>
2024-09-25 17:23:46 +0700

    finalll

--- likeArth/src/assets/components/hero.jsx
+++ likeArth/src/assets/components/hero.jsx
@@ -63,7 +63,7 @@ export default function Hero() {
                             </motion.div>
                         </div>
                         {/* contact us */}
-                        <motion.div whileInView={{opacity: [0, 1]}} transition={{delay: 2.2, duration: 1, ease: "easeInOut" }} viewport={{ once: true }} className="px-7 sm:px-[58px] md:px-[165px] lg:px-[280px] xl:px-[270px] container mx-auto max-w-[1120px] flex items-center justify-center">
+                        <motion.div whileInView={{opacity: [0, 1]}} transition={{delay: 2.5, duration: 1, ease: "easeInOut" }} viewport={{ once: true }} className="px-7 sm:px-[58px] md:px-[165px] lg:px-[280px] xl:px-[270px] container mx-auto max-w-[1120px] hidden md:flex items-center justify-center">
                             <motion.div whileInView={{width: widthAnimation}} transition={{delay: 2.4, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className="bg-white rounded-[0.938rem] pl-[17px] py-1 xl:py-0 justify-between items-center flex relative z-10 overflow-hidden">
                                 {/* invisible place holder */}
                                 <p className='invisible py-[0.1rem] md:py-[0.6rem] leading-[28px]'>1</p>
@@ -79,6 +79,27 @@ export default function Hero() {
                                 </div>
                                 <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%] absolute left-[20px]"></div>
                                 <motion.a href="https://cal.com/likearthstudio/30min" target='_blank' className='py-[10px] md:py-[17px] xl:py-[13px] w-[100px] md:w-[170px] text-center text-white text-[0.875rem] bg-primary hover:bg-[#04525b] transition-all duration-500 ease-in-out rounded-[0.938rem] absolute right-0 z-30'>Book a Call</motion.a>
+                            </motion.div>
+                        </motion.div>
+                        {/* contack us mobile */}
+                        <motion.div whileInView={{y: [25, 0] }} transition={{delay: 0.9, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className='flex md:hidden justify-center items-center'>
+                            <motion.div whileInView={{opacity: [0, 1] }} transition={{delay: 1, duration: 1.5, ease: "easeInOut" }} viewport={{ once: true }} className="flex items-center justify-center gap-1 md:gap-4 xl:gap-[0.625rem] bg-white pl-[0.75rem] rounded-[0.938rem] relative w-auto transition-all duration-500 ease-in-out">
+                                <div className='flex items-center justify-center gap-[0.625rem]'>
+                                    {/* icon pulse invis */}
+                                    <div className="invisible flex items-center justify-center bg-red-500 w-[1rem] h-[1rem] rounded-[50%] isolate">
+                                        <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%]"></div>
+                                    </div>
+                                    {/*attention */}
+                                    <motion.p  whileInView={{opacity: [0, 1], fontSize: ['0.1rem', '0.875rem'] }} transition={{delay: 2.2, duration: 1, ease: "easeInOut" }} viewport={{ once: true }} className='py-[0.1rem] md:py-[0.6rem] leading-[28px] mr-2'><span className="text-primary">1 Slot available!</span> <span className='hidden md:inline-block text-[#676767]'>Let's talk</span> <span className='hidden xl:inline-block text-[#676767]'>to get your project started.</span></motion.p>
+                                </div>
+                                {/* icon pulse */}
+                                <div className="absolute left-[0.75rem] flex items-center justify-center bg-primary bg-opacity-25 w-[1rem] h-[1rem] rounded-[50%] animate-scale-down-infinite">
+                                </div>
+                                <div className="w-[0.625rem] h-[0.625rem] bg-primary rounded-[50%] absolute left-[0.90rem]"></div>
+                                {/* btn */}
+                                <a href="https://cal.com/likearthstudio/30min" target="_blank">
+                                    <Btn title="Book a Call" style="flex justify-center items-center relative bg-primary hover:bg-[#04525b] transition-all duration-500 ease-in-out text-white border border-primary overflow-hidden rounded-[0.938rem] py-[0.719rem] px-[1.5rem] absolute"/>                               
+                                </a>
                             </motion.div>
                         </motion.div>
                     </div>
