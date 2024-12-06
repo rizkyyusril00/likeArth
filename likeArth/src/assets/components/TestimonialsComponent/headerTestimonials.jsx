e62a5aaf3d080da0403f7511da9e55cfb795d8c6
rizkyyusril <rizkyyusril74@gmail.com>
2024-12-06 17:31:27 +0700

    test tesetimoni

--- likeArth/src/assets/components/TestimonialsComponent/headerTestimonials.jsx
+++ likeArth/src/assets/components/TestimonialsComponent/headerTestimonials.jsx
@@ -1,10 +1,20 @@
-import Testimoni from "../../img/testimoni1.svg";
 import Lottie from "lottie-react";
 import AnimationDino from "../../lottie/testimoni.json";
+import { useMemo } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function HeaderTestimonials() {
+  // Lazy load Lottie when in viewport
+  const { ref, inView } = useInView({ triggerOnce: true });
+
+  // Memoize Lottie animation data
+  const memoizedAnimationData = useMemo(() => AnimationDino, []);
+
   return (
-    <div className="testimonials-box flex flex-col xl:flex-row justify-center xl:justify-between items-center pt-[5rem] mb-[2.5rem]">
+    <div
+      className="testimonials-box flex flex-col xl:flex-row justify-center xl:justify-between items-center pt-[5rem] mb-[2.5rem]"
+      ref={ref} // Attach ref to trigger lazy load
+    >
       {/* box1 */}
       <div className="flex flex-col items-center justify-center xl:items-start xl:justify-start w-full xl:w-[50%] gap-[1.5rem]">
         <div className="flex flex-col justify-center items-center xl:items-start xl:justify-start gap-[0.5rem]">
@@ -42,23 +52,25 @@ export default function HeaderTestimonials() {
         </p>
       </div>
       {/* box2 */}
-      {/* <div
-        className="relative -mt-10 md:-mt-14 lg:-mt-0"
-        data-aos="fade-left"
-        data-aos-once="true"
-        data-aos-duration="1000"
-        data-aos-easing="ease-in-out"
-        data-aos-delay="500"
-      >
-        <Lottie
-          animationData={AnimationDino}
-          loop={true}
-          autoPlay={true}
-          style={{ width: "100%", height: "100%" }}
-          className="w-full h-full"
-        />
-        <div className="w-[200px] h-[40px] absolute bottom-0 right-0 bg-white"></div>
-      </div> */}
+      {inView && ( // Render Lottie only when in viewport
+        <div
+          className="relative -mt-10 md:-mt-14 lg:-mt-0"
+          data-aos="fade-left"
+          data-aos-once="true"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="500"
+        >
+          <Lottie
+            animationData={memoizedAnimationData}
+            loop={true}
+            autoPlay={true}
+            style={{ width: "100%", height: "100%" }}
+            className="w-full h-full"
+          />
+          <div className="w-[200px] h-[40px] absolute bottom-0 right-0 bg-white"></div>
+        </div>
+      )}
     </div>
   );
 }
