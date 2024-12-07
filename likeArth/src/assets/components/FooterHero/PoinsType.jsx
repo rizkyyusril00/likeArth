ad3cb3fe3fcd4d58c1ed72bffcf0aee96a2bf158
rizkyyusril <rizkyyusril74@gmail.com>
2024-12-07 09:08:16 +0700

    test

--- likeArth/src/assets/components/FooterHero/PoinsType.jsx
+++ likeArth/src/assets/components/FooterHero/PoinsType.jsx
@@ -1,22 +1,27 @@
 import { useTypewriter } from "react-simple-typewriter";
+import React from "react";
 
-export default function PoinsType() {
-  // type animation
+// Tidak perlu langsung membungkus dengan memo
+const PoinsType = () => {
   const [Poins] = useTypewriter({
     words: [
       "Experienced Designer",
-      "Your satisfactions is our priority",
+      "Your satisfaction is our priority",
       "Unlimited design requests & revisions",
     ],
     loop: false,
-    typeSpeed: 25,
-    deleteSpeed: 25,
-    delaySpeed: 1500,
+    typeSpeed: 10,
+    deleteSpeed: 10,
+    delaySpeed: 1000,
   });
+
   return (
     <div className="flex justify-center items-center gap-[0.5rem] lg:hidden">
       <i className="bi bi-check-circle text-primary text-[1rem]"></i>
       <p className="text-[1rem] text-[#676767] text-center">{Poins}</p>
     </div>
   );
-}
+};
+
+// Bungkus memo saat ekspor
+export default React.memo(PoinsType);
