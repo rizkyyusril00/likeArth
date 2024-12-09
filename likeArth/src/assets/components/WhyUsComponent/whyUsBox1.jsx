d9ef3445a34ac7165458dbbac6fb3253a5fe355f
rizkyyusril <rizkyyusril74@gmail.com>
2024-12-09 14:45:47 +0700

    test

--- likeArth/src/assets/components/WhyUsComponent/whyUsBox1.jsx
+++ likeArth/src/assets/components/WhyUsComponent/whyUsBox1.jsx
@@ -3,8 +3,6 @@ import AnimationDino from "../../lottie/rexUpdate.json";
 import { useMemo, useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import WhyUs from "../../img/why_us/whyus1.svg";
-import AOS from "aos";
-import "aos/dist/aos.css"; // Ini untuk mengimport CSS AOS
 
 export default function WhyUsBox1() {
   const [isMobile, setIsMobile] = useState(false);
@@ -53,22 +51,14 @@ export default function WhyUsBox1() {
       {inView &&
         (isMobile ? (
           // Mobile: Render image
-          <div
-            className="relative order-2 md:order-1 -mt-10 md:-mt-0"
+          <figure
             data-aos="fade-left"
             data-aos-once="true"
             data-aos-duration="1000"
             data-aos-easing="ease-in-out"
           >
-            <Lottie
-              animationData={memoizedAnimationData}
-              loop={true}
-              autoPlay={true}
-              style={{ width: "100%", height: "100%" }}
-              className="w-full h-full"
-            />
-            <div className="w-[200px] h-[40px] absolute bottom-0 right-0 bg-white"></div>
-          </div>
+            <img src={WhyUs} alt="Why Us illustration for mobile" />
+          </figure>
         ) : (
           // Desktop/Tablet: Render Lottie
           <div
